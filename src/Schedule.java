5dd301055d0c364f9e9223ee1ebe5d7e9b45cd77
Dudrop <du.yuechen.kelvin@gmail.com>
2020-01-11 17:42:25 -0500

    Created arraylist for courses created JPanels for UI and the calendar

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,6 +1,6 @@
 /* [Schedule.java]
  * The main class for the optimizer, also the frame for UI
- * Albert Quon, Kelvin Du, Garvin Hui, Gordon
+ * Albert Quon, Kelvin Du, Garvin Hui, Gordon Tang
  * 2020/01/11
  */
 //imports
@@ -16,22 +16,56 @@ import java.awt.event.*;
     building name/abbreviation
     time
  */
+// graphic import
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Schedule extends JFrame {
+
+    // initialize J elements
     static JFrame window;
-    static JPanel panel;
+    static JPanel ioPanel;
+    static JPanel schPanel;
 
     public static void main(String[] args){
         System.out.println("Schedule");
         window = new Schedule();
+        //create an array list of type string
+        ArrayList<String> courses = new ArrayList<String>();
+
+        // add array list elements
+        courses.add("COMP 1405");
+        courses.add("COMP 1406");
+        courses.add("COMP 1805");
+        courses.add("COMP 2804");
+        courses.add("MATH 1007");
+        courses.add("MATH 2007");
+        courses.add("MATH 1104");
+        courses.add("STAT 2507");
+        courses.add("ECON 1001");
+        courses.add("ECON 1002");
+        courses.add("STAT 2509");
     }
 
     Schedule() {
         super("CSO");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        window.setResizable(false);
+
+        // set up the panel to display questions
+        ioPanel = new UIPanel();
+        this.add(new UIPanel());
+
+        // set up the panel to display the schedule
+        schPanel = new SchedulePanel();
+        this.add(new SchedulePanel());
+    }
+    private class UIPanel extends JPanel {
+
+    }
+    private class SchedulePanel extends JPanel {
 
     }
 }
